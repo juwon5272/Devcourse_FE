c79d09a8e4348e6e7f06392eca038012e7f1a226
juwon5272 <jackson5272@naver.com>
2024-06-11 23:29:05 +0900

    feat : theme switcher (light,dark mode) button

---
+++ book-store/src/components/header/ThemeSwitcher.tsx
@@ -0,0 +1,11 @@
+// import { useContext } from 'react';
+// import { ThemeContext } from '../../context/themeContext';
+
+function ThemeSwitcher() {
+  //   const { themeName, toggleTheme } = useContext(ThemeContext);
+
+  //   return <button onClick={toggleTheme}>{themeName}</button>;
+  return <button>테마스위치</button>;
+}
+
+export default ThemeSwitcher;
