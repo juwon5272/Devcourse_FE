54da941c8646f9e7d1a687cba2e7c7ab2f0439af
juwon5272 <jackson5272@naver.com>
2024-06-12 23:46:22 +0900

    feat : add theme switcher and layout

--- book-store/src/App.tsx
+++ book-store/src/App.tsx
@@ -1,13 +1,18 @@
-import Home from "./pages/Home.tsx";
-import { GlobalStyle } from "./style/global.ts";
-import { ThemeProvider } from "styled-components";
-import { light } from "./style/theme.ts";
+import Home from './pages/Home.tsx';
+import { GlobalStyle } from './style/global.ts';
+import { ThemeProvider } from 'styled-components';
+import { light, dark } from './style/theme.ts';
+import Layout from './components/layout/layout.tsx';
+import ThemeSwitcher from './components/header/ThemeSwitcher.tsx';
 
 function App() {
   return (
     <ThemeProvider theme={light}>
-      <GlobalStyle />
-      <Home />
+      <GlobalStyle themeName="light" />
+      <ThemeSwitcher />
+      <Layout>
+        <Home />
+      </Layout>
     </ThemeProvider>
   );
 }
