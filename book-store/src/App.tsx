d06388cf384c232b79eddbfeebcad6cd909846dc
juwon5272 <jackson5272@naver.com>
2024-06-16 17:40:03 +0900

    feat : 라우팅 설정

--- book-store/src/App.tsx
+++ book-store/src/App.tsx
@@ -1,13 +1,31 @@
 import Home from "./pages/Home.tsx";
 import Layout from "./components/layout/layout.tsx";
 import { BookStoreThemeProvider } from "./context/themeContext.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./components/common/Error.tsx";
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+    errorElement: <Error />,
+  },
+  {
+    path: "/books",
+    element: <Layout> 도서 목록</Layout>,
+    errorElement: <Error />,
+  },
+];
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <BookStoreThemeProvider>
-      <Layout>
-        <Home />
-      </Layout>
+      <RouterProvider router={router} />
     </BookStoreThemeProvider>
   );
 }
